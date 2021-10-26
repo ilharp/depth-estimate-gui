abd85fad8e49d87a16c886a7e8e7ffeedc59e234
Il Harper <afanyiyu@hotmail.com>
2021-10-26 18:34:44 +0800

    feat: init project

---
+++ src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+
+namespace DepthEstimateGui
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            // Magic - github.com/AvaloniaUI/Avalonia/issues/1934
+            RxApp.MainThreadScheduler = AvaloniaScheduler.Instance;
+
+            // Build Avalonia app
+            BuildAvaloniaApp()
+                .StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseSkia()
+                .LogToTrace();
+    }
+}
