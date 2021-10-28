ff5424839b1016b1cf907099e6dfd1a3c61bda25
Il Harper <afanyiyu@hotmail.com>
2021-10-29 05:05:53 +0800

    feat: add process env

--- src/Core/Process.cs
+++ src/Core/Process.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reactive.Subjects;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,13 +40,26 @@ namespace DepthEstimateGui.Core
 
             // Prepare paths
             string rootPath = AppDomain.CurrentDomain.BaseDirectory;
+            string toolsPath = Path.Combine(rootPath, "tools");
             string pythonPath = Path.Combine(
                 rootPath,
-                isWindows ?
-                    "tools\\python.exe" :
-                    "tools/bin/python");
+                isWindows ? "tools\\python.exe" : "tools/bin/python");
             string corePath = Path.Combine(rootPath, "tools", settings.Core);
             string outputName = $"{Id}.{settings.Ext}";
+
+            // Prepare environment variables
+            string pathValue = isWindows
+                ? $"{toolsPath};{Path.Combine(toolsPath, "Library", "bin")};{Path.Combine(toolsPath, "Scripts")};"
+                : $"{Path.Combine(toolsPath, "bin")}:";
+            pathValue += Environment.GetEnvironmentVariable("PATH");
+            IDictionary envOrigin = Environment.GetEnvironmentVariables();
+            Dictionary<string, string?> env = envOrigin
+                .Cast<DictionaryEntry>()
+                .ToDictionary(entry => (string)entry.Key, entry => (string?)entry.Value);
+            if (env.ContainsKey("Path"))
+                env["Path"] = pathValue;
+            if (env.ContainsKey("PATH"))
+                env["PATH"] = pathValue;
 
             GraphicStorage.EnsureDirectoryExists();
 
@@ -54,6 +70,7 @@ namespace DepthEstimateGui.Core
                 .WithArguments(
                     $"{(isWindows ? "" : "./")}1kgen.py {InputName} {outputName} {settings.ColorMap}")
                 .WithWorkingDirectory(corePath)
+                .WithEnvironmentVariables(env)
                 .WithValidation(CommandResultValidation.None)
                 .WithStandardOutputPipe(PipeTarget.ToStringBuilder(output))
                 .WithStandardErrorPipe(PipeTarget.ToStringBuilder(output))
